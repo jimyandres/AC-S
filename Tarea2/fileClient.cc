1694768df920837a5a06b8928504bfc5549ff754
JohanQuiroga <quirogacj@utp.edu.co>
2017-02-19 11:32:14 -0500

    initial commit for branch FileServer_V2

---
+++ Tarea2/fileClient.cc
@@ -0,0 +1,363 @@
+#include <iostream>
+#include <string>
+#include <zmqpp/zmqpp.hpp>
+#include <sys/stat.h>
+
+#include <openssl/md5.h>
+
+#include <unistd.h> 		//Get the user id of the current user
+#include <sys/types.h>
+#include <pwd.h>			//Get the password entry (which includes the home directory) of the user
+
+using namespace std;
+using namespace zmqpp;
+
+void printChecksum (unsigned char * check_sum) {
+	char mdString[33];
+ 
+    for(int i = 0; i < 16; i++)
+         sprintf(&mdString[i*2], "%02x", (unsigned int)check_sum[i]);
+ 
+    printf("%s\n", mdString);
+}
+
+void printMenu() {
+	cout << "\n\n***********************************\n";
+	cout << "Enter action to perform: \n\tList_files\n\tDownload\n\tUpload\n\tDelete\n\tExit\n";
+	cout << "\n";
+}
+
+void getFileName(char fname[100], char file_name[100]) {
+	char *temp_pt = strrchr(fname, '/');
+	if(temp_pt == NULL) {
+		strcpy (file_name, fname);
+	} else {
+		//cout << strlen(fname) << endl;
+		size_t pos = temp_pt-fname+1, j=0;
+		while(pos < strlen(fname)) {
+			file_name[j] = fname[pos];
+			pos++;
+			j++;
+		}
+		file_name[j] = '\0';
+	}
+	//cout << "file: " << file_name << endl;
+}
+
+void downloadFile(socket &s, string op, string username) {
+	char fname[100];
+	string file_name, path;
+	message metadata_file, answer, data_request;
+	size_t size;
+	char *data, *data_md5;
+	FILE* f;
+	int CHUNK_SIZE;
+
+	unsigned char check_sum[MD5_DIGEST_LENGTH];
+	unsigned char *received_check_sum;
+
+	cout << "Enter file name: \n";
+	cin.getline(fname, sizeof(fname));
+	cin.getline(fname, sizeof(fname));
+
+	metadata_file << op << username << fname;
+	s.send(metadata_file);
+
+	s.receive(answer);
+
+	answer.reset_read_cursor();
+
+	if(answer.get(0) == "Error") {
+		if(atoi(answer.get(1).c_str()) == 0) {
+			cout << "\nThe file requested does not exist or couldn't be read!!" << endl;
+			return;
+		}
+	}
+
+	answer >> size;
+	answer >> file_name;
+	answer >> CHUNK_SIZE;
+
+	received_check_sum = (unsigned char *)answer.raw_data(3);
+
+
+	data_request << "ok";
+	s.send(data_request);
+
+	cout << "File size: " << size << endl;
+
+	//Check if "Downloads/" dir exists,
+	//	if not, it's created.
+	struct passwd *pw = getpwuid(getuid());
+	const char *homedir = pw->pw_dir;
+
+	path = (string)homedir + "/Descargas/Downloads/";
+
+	struct stat sb;
+	lstat(path.c_str(), &sb);
+
+	if(!S_ISDIR(sb.st_mode)) {
+		string url = "mkdir -p " + (string)path;
+		const char * directory =  url.c_str();
+		system(directory);
+	}
+
+	path.append(file_name);
+
+	cout << "Path: " << path << endl;
+
+	size_t total = 0;
+	size_t chunks = 0;
+
+
+	cout << "Saving to file..." << endl;
+
+	while(true){
+
+		if (!s.receive(answer))
+			break;
+
+		if (total == 0)
+			f = fopen(path.c_str(), "wb");
+		else
+			f = fopen(path.c_str(), "a+");
+		assert(f);
+		fflush(f);
+		fseek(f, 0L, SEEK_END);
+
+		chunks++;
+		size = answer.size(0);
+		if (size == 0)
+			break;
+		data = (char*)answer.raw_data();
+		fwrite(data, 1, size, f);
+		total += size;
+
+		fclose(f);
+
+		data_request << "ok";
+		s.send(data_request);
+
+		if ((int)size < CHUNK_SIZE){
+			break;
+		}
+
+	}
+
+	f = fopen(path.c_str(), "rb");
+	assert(f);
+	fflush(f);
+	fseek(f, 0L, SEEK_SET);
+
+	data_md5 = (char*) malloc (sizeof(char)*total);
+	assert(data_md5);
+
+	size = fread(data_md5, 1, total, f);
+
+	MD5((unsigned char *)data_md5, size, (unsigned char *)&check_sum);
+	
+	cout << "Calculated check sum: ";
+	printChecksum(check_sum);
+
+	cout << "Received Check sum: ";
+	printChecksum(received_check_sum);
+
+	if(memcmp(received_check_sum, check_sum, MD5_DIGEST_LENGTH)) {
+		cout << "\nThe Checksum failed, please try again!!" << endl;
+	}
+	else cout << "File saved successfully\n";
+	fclose(f);
+	free(data_md5);
+	s.receive(answer);
+	if(answer.get(0) != "ok") {
+		cout << "Lost Connection!";
+	}
+}
+
+void uploadFile(socket &s, string op, string username) {
+	FILE* f;
+	char *data;
+	size_t size;
+	char fname[100], file_name[100];
+	message metadata_message, server_answer, data_message;
+
+	unsigned char check_sum[MD5_DIGEST_LENGTH];
+
+	cout << "Enter file name: \n";
+	cin.getline(fname, sizeof(fname));
+	cin.getline(fname, sizeof(fname));
+
+	getFileName(fname, file_name);
+
+	if(!(f = fopen(fname, "rb"))) {
+		cout << "\nThe file requested does not exist or couldn't be read!!" << endl;
+		return;
+	}
+	fflush(f);
+
+	fseek(f, 0L, SEEK_END);
+	long sz = ftell(f);
+	cout << "\nFile size (bytes): " << sz << endl;
+	fseek(f, 0L, SEEK_SET);
+
+	cout << "File to upload: " << file_name << endl;
+
+	data = (char*) malloc (sizeof(char)*sz);
+	assert(data);
+
+	size = fread(data, 1, sz, f);
+
+	metadata_message << op << username << file_name << size;
+
+	metadata_message.push_back(data, size);
+
+	/* first argument needs to be an unsigned char pointer
+     * second argument is number of bytes in the first argument
+     * last argument is our buffer, which needs to be able to hold
+     * the 16 byte result of the MD5 operation */
+    MD5((unsigned char *)data, size, (unsigned char *)&check_sum);
+
+    cout << "Calculated check sum: ";
+	printChecksum(check_sum);
+
+    metadata_message.push_back(check_sum, MD5_DIGEST_LENGTH);
+
+
+	if(s.send(metadata_message)) {
+		cout << "Message containing \""<< file_name << "\" has been sended successfully\n";
+	} else {
+		cout << "Message containing \""<< file_name << "\" hasnt been sended successfully\n";
+	}
+
+	s.receive(server_answer);
+
+	if(server_answer.get(0) == "Error") {
+		if(atoi(server_answer.get(1).c_str()) == 0) {
+			cout << "\nThe file requested does not exist or couldn't be read!!" << endl;
+			return;
+		} else if(atoi(server_answer.get(1).c_str()) == 0) {
+			cout << "\nThe Checksum failed, please try again!!" << endl;
+		}
+	}
+
+	if(server_answer.get(0) == "ok") {
+		cout << "File has been uploaded successfully!!" << endl;
+	}
+
+	fclose(f);
+	free(data);
+}
+
+void listFiles(socket &s, string op, string username) {
+	message request, response;
+	string files;
+
+	request << op << username;
+	s.send(request);
+	s.receive(response);
+
+	response >> files;
+	cout << "Files:" << endl;
+	cout << files;
+}
+
+void deleteFile(socket &s, string op, string username) {
+	message request, response;
+	string filename, status;
+
+	cout << "Enter File name: \n";
+	cin >> filename;
+
+	request << op << username << filename;
+	s.send(request);
+	s.receive(response);
+
+	response >> status;
+	cout << "Deleting file \"" << filename << "\": " << status << endl;
+}
+
+int main() {
+
+	message login, response, create_user;
+	string op, create, answer;
+	char username[40];
+	string password = "";
+	int access;
+
+	cout << "This is the client\n";
+
+	context ctx;
+	socket s(ctx, socket_type::req);
+
+	cout << "Connecting to tcp port 5555\n";
+	s.connect("tcp://localhost:5555");
+
+	while(true) {
+		cout << "Create new account (yes/no): ";
+		cin >> create;
+
+		if (create == "yes" || create == "y") {
+			cout << "Enter User name: \n";
+			cin >> username;
+
+			cout << "Enter Password: \n";
+			cin >> password;
+
+	        create_user << "CreateUser" << username << password;
+	        s.send(create_user);
+	        s.receive(response);
+
+	        response >> op;
+	        response >> answer;
+
+	        cout << answer;
+
+	        if (op == "Ok") {
+	        	break;
+	        }
+
+	    } else if (create == "no" || create == "n") {
+	    	cout << "Enter User name: \n";
+	    	cin >> username;
+
+	    	cout << "Enter Password: \n";
+	    	cin >> password;
+	    	break;
+
+	    } else cout << "Invalid option!\n";
+	}
+
+	login << "Login" << username << password;
+
+	s.send(login);
+	s.receive(response);
+
+	response >> access;
+
+	if (access) {
+		while(true) {
+			printMenu();
+			cout <<  "Enter action to perform: \n";
+			cin >> op;
+
+			if(op == "Upload" || op == "upload" || op == "up") {
+				uploadFile(s, "Upload", username);
+			} else if(op == "Download" || op == "download" || op == "down") {
+				downloadFile(s, "Download", username);
+			} else if(op == "List_files" || op == "list_files" || op == "ls") {
+				listFiles(s, "List_files", username);
+			} else if(op == "Delete" || op == "delete" || op == "del") {
+				deleteFile(s, "Delete", username);
+			} else if(op == "Exit" || op == "exit" || op == "ex") {
+				break;
+			} else {
+				cout << "\nInvalid option, please enter one of the listed options!\n";
+			}
+		}
+	}
+	else cout << "\nUser not found!!\n\n";
+
+	cout << "Finished\n";
+	s.close();
+	return 0;
+}
---
+++ Tarea2/fileServer.cc
@@ -0,0 +1,324 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <zmqpp/zmqpp.hpp>
+#include <dirent.h>
+#include <sys/stat.h>
+
+#include <jsoncpp/json/json.h>
+#include <jsoncpp/json/writer.h>
+
+#include <openssl/md5.h>
+
+#define CHUNK_SIZE 250000
+
+using namespace std;
+using namespace zmqpp;
+
+void printChecksum (unsigned char * check_sum) {
+	char mdString[33];
+ 
+    for(int i = 0; i < 16; i++)
+         sprintf(&mdString[i*2], "%02x", (unsigned int)check_sum[i]);
+ 
+    printf("%s\n", mdString);
+}
+
+void listFiles(message &m, message &response, socket &s) {
+	string files, user, path;
+
+	m >> user;
+
+	DIR *dir;
+	struct dirent *ent;
+	path = "Uploads/" + user;
+	const char * url =  path.c_str();
+	if ((dir = opendir (url)) != NULL) {
+		/* print all the files and directories within directory */
+		while ((ent = readdir (dir)) != NULL) {
+			files += "\t";
+			files += ent->d_name;
+			files += "\n";
+			//printf ("%s\n", ent->d_name);
+		}
+		files += "\n";
+		closedir (dir);
+
+	} else {
+	  /* could not open directory */
+		perror ("");
+	  //return EXIT_FAILURE;
+	}
+	response << files;
+	s.send(response);
+
+}
+
+void deleteFile(message &m, message &response, socket &s) {
+	string user, filename, l;
+
+	m >> user;
+	m >> filename;
+
+	l = "Uploads/" + user + "/" + filename;
+
+	cout << "Deleting: " + l;
+	const char * location =  l.c_str();
+
+	if( remove( location ) != 0 ){
+		response << "Error deleting file";
+	}
+	else {
+		response << "File successfully deleted";
+	}
+	s.send(response);
+}
+
+void uploadFile(message &client_request, message &server_response, socket &s) {
+	string fname, path, username;
+	size_t size;
+	char * data;
+
+	unsigned char check_sum[MD5_DIGEST_LENGTH];
+	unsigned char *received_check_sum;
+
+	client_request >> username;
+	client_request >> fname;
+	client_request >> size;
+
+	cout << "File to be uploaded: " << fname << " of size (bytes): " << size << endl;
+
+	path = "Uploads/" + username + "/";
+
+	path.append(fname);
+
+	cout << "Path: " << path.c_str() << endl;
+
+	FILE *f = fopen(path.c_str(), "wb");
+	assert(f);
+	fflush(f);
+	fseek(f, 0L, SEEK_SET);
+
+	data = (char*)client_request.raw_data(4);
+	received_check_sum = (unsigned char *)client_request.raw_data(5);
+
+	MD5((unsigned char *)data, size, (unsigned char *)&check_sum);	
+
+	//cout << "Received check sum: ";
+	//printChecksum(received_check_sum);
+
+	cout << "Calculated check sum: ";
+	printChecksum(check_sum);
+
+	if(memcmp(received_check_sum, check_sum, MD5_DIGEST_LENGTH)) {
+		cout << "Error" << endl;
+		server_response << "Error" << 1;
+		s.send(server_response);
+		return;
+	}
+
+	cout << "Saving file...\n";
+
+	fwrite(data, 1, size, f);
+	fclose(f);
+
+	server_response << "ok";
+	s.send(server_response);
+
+	cout << "File saved!" << endl;
+}
+
+void downloadFile(message &client_request, message &server_r, socket &s) {
+	FILE* f;
+	char *data_md5;
+	size_t size, size_md5;
+	long sz;
+
+	message ok, server_response;
+
+	unsigned char check_sum[MD5_DIGEST_LENGTH];
+
+	string fname, path, username;
+	client_request >> username;
+	client_request >> fname;
+
+	path = "Uploads/" + username + "/";
+
+	path.append(fname);
+
+	if(!(f = fopen(path.c_str(), "rb"))) {
+		server_response << "Error" << 0;
+		s.send(server_response);
+		return;
+	}
+
+	fflush(f);
+
+	fseek(f, 0L, SEEK_END);
+	sz = ftell(f);
+	cout <<"File size in bytes: " << sz << endl;
+	fseek(f, 0L, SEEK_SET);
+
+	data_md5 = (char*) malloc (sizeof(char)*sz);
+
+	size_md5 = fread(data_md5, 1, sz, f);
+
+
+	server_response << sz << fname << CHUNK_SIZE;
+
+	MD5((unsigned char *)data_md5, size_md5, (unsigned char *)&check_sum);
+	free(data_md5);
+
+	cout << "Calculated check sum: ";
+	printChecksum(check_sum);
+
+	server_response.push_back(check_sum, MD5_DIGEST_LENGTH);
+
+	s.send(server_response);
+	s.receive(ok);
+	if(ok.get(0) != "ok"){
+		cout << "NO OK" << endl;
+		return;
+	}
+
+	fseek(f, 0L, SEEK_SET);
+
+	while(true){
+
+		char *data = (char *)malloc (CHUNK_SIZE);
+		assert(data);
+		size = fread (data, 1, CHUNK_SIZE, f);
+
+		if (size == 0) {
+			break;
+		}
+
+		server_response.push_back(data, size);
+		if(!s.send(server_response))
+			cout << "Message that contains \"Data\" hasnt been sended successfully\n";
+		s.receive(ok);
+		if (ok.get(0) != "ok")
+			break;
+		free(data);
+
+	}
+	server_response << "ok";
+	s.send(server_response);
+	fclose(f);
+}
+
+void createUser(message &m, message &response, socket &s) {
+	string user, password, url;
+
+	m >> user;
+	m >> password;
+
+	Json::Value root;   // will contains the root value after parsing.
+	Json::Reader reader;
+	Json::StyledStreamWriter writer;
+	std::ifstream test("users.json");
+	bool parsingSuccessful = reader.parse( test, root );
+	if ( !parsingSuccessful )
+	{
+        // report to the user the failure and their locations in the document.
+		std::cout  << "Failed to parse configuration: "<< reader.getFormattedErrorMessages();
+	}
+	if (root[user]["password"] == password) {
+		response << "Failed" << "Username \"" + user + "\" already exists!";
+	}
+	else {
+		root[user]["password"] = password;
+		test.close();
+		ofstream test1("users.json");
+		cout << "New user created: " << user << endl;
+		writer.write(test1,root);
+
+		url = "mkdir -p Uploads/" + user;
+		const char * directory =  url.c_str();
+		system(directory);
+		response << "Ok" << "Username \"" + user + "\" was created!";
+	}
+
+	s.send(response);
+
+}
+
+void verifyUser(message &m, message &response, socket &s) {
+
+	string user, password;
+
+	m >> user;
+	m >> password;
+
+	Json::Value root;   // will contains the root value after parsing.
+	Json::Reader reader;
+	Json::StyledStreamWriter writer;
+	std::ifstream test("users.json");
+	bool parsingSuccessful = reader.parse( test, root );
+	if ( !parsingSuccessful )
+	{
+        // report to the user the failure and their locations in the document.
+		std::cout  << "Failed to parse configuration: "<< reader.getFormattedErrorMessages();
+	}
+	if (root[user]["password"] == password) {
+		response << 1;
+	}
+	else response << 0;
+
+	s.send(response);
+}
+
+void messageHandler(message &client_request, message &server_response, socket &s) {
+	string op;
+	client_request >> op;
+
+	cout << "Option: " << op << endl;
+
+	if(op == "CreateUser") {
+		createUser(client_request, server_response, s);
+	} else  if(op == "Login") {
+		verifyUser(client_request, server_response, s);
+	} else  if(op == "Upload") {
+		uploadFile(client_request, server_response, s);
+	} else if(op == "Download") {
+		downloadFile(client_request, server_response, s);
+	} else if(op == "List_files") {
+		listFiles(client_request, server_response, s);
+	} else if(op == "Delete") {
+		deleteFile(client_request, server_response, s);
+	} else {
+		server_response << "Error";
+	}
+}
+
+int main() {
+	cout << "This is the server\n"; 
+
+	context ctx;  
+	socket s(ctx, socket_type::rep);
+
+	cout << "Binding socket to tcp port 5555\n";
+	s.bind("tcp://*:5555");
+
+	struct stat sb;
+	lstat("Uploads/", &sb);
+
+	if(!S_ISDIR(sb.st_mode)) {
+		string url = "mkdir -p Uploads";
+		const char * directory =  url.c_str();
+		system(directory);
+	}
+
+	while(true) {
+		cout << "Waiting for message to arrive!\n";
+
+		message client_request, server_response;
+		s.receive(client_request);
+
+		cout << "Message received!\n";
+
+		messageHandler(client_request, server_response, s);
+	}
+
+	return 0;
+}
